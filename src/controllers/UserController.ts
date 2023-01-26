c0092e4046bc2a0ee8e3e2aa69d6798f2ed0facb
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-01-26 14:50:06 +0700

    update

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,10 +6,28 @@ const bcrypt = require("bcrypt");
 
 class UserController implements IController {
   index = async (req: Request, res: Response): Promise<Response> => {
-    const filters = req.query.filters ? JSON.parse(`${req.query.filters}`) : [];
-    console.log(filters);
+    const filters: any = req.query.filters
+      ? JSON.parse(`${req.query.filters}`)
+      : [];
+    const fields: any = req.query.fields
+      ? JSON.parse(`${req.query.fields}`)
+      : ["name"];
+
+    // Fungsi set field yang ditampilkan\
+    let setField: any = {};
+    for (const field of fields) {
+      console.log(field);
+      if (field != "password") {
+        setField[field] = 1;
+      }
+    }
+    // End
+    const order_by: any = req.query.order_by || { updatedAt: -1 };
+    const last_id: number | string = parseInt(`${req.query.lastId}`) || 0;
+    const limit: number | string = parseInt(`${req.query.limit}`) || 10;
     try {
-      const users = await User.find();
+      const getAll = await User.find({}).count();
+      const users = await User.find({}, setField).limit(limit).sort(order_by);
       const stateFilter: any[] = [
         {
           name: "name",
@@ -42,9 +60,14 @@ class UserController implements IController {
           targetdata: "users",
         },
       ];
-      return res
-        .status(200)
-        .json({ status: 200, data: users, filters: stateFilter });
+      return res.status(200).json({
+        status: 200,
+        limit,
+        last_id,
+        total: getAll,
+        data: users,
+        filters: stateFilter,
+      });
     } catch (error) {
       return res.status(400).json({ status: 400, data: error });
     }
