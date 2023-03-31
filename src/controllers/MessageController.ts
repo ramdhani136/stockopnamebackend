9e5fcec671342479fa56f6c89b83130c6dda2cf8
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-03-31 09:28:17 +0700

    update

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -1,14 +1,90 @@
 import { Request, Response } from "express";
+import { IStateFilter } from "../Interfaces";
+import { TypeOfState } from "../Interfaces/FilterInterface";
 import { ChatModel, User } from "../models";
 import MessageModel from "../models/MessageModel";
+import FilterQuery, { ISearch } from "../utils/FilterQuery";
 
 class MessageController {
   allMessages = async (req: Request | any, res: Response): Promise<any> => {
+    const stateFilter: IStateFilter[] = [
+      {
+        name: "_id",
+        operator: ["=", "!=", "like", "notlike"],
+        typeOf: TypeOfState.String,
+      },
+      {
+        name: "sender",
+        operator: ["=", "!=", "like", "notlike"],
+        typeOf: TypeOfState.String,
+      },
+      {
+        name: "content",
+        operator: ["=", "!=", "like", "notlike"],
+        typeOf: TypeOfState.String,
+      },
+      {
+        name: "chat",
+        operator: ["=", "!=", "like", "notlike"],
+        typeOf: TypeOfState.String,
+      },
+      {
+        name: "chat._id",
+        operator: ["=", "!=", "like", "notlike"],
+        typeOf: TypeOfState.String,
+      },
+      {
+        name: "updatedAt",
+        operator: ["=", "!=", "like", "notlike", ">", "<", ">=", "<="],
+        typeOf: TypeOfState.Date,
+      },
+      {
+        name: "createdAt",
+        operator: ["=", "!=", "like", "notlike", ">", "<", ">=", "<="],
+        typeOf: TypeOfState.Date,
+      },
+    ];
     try {
-      const messages = await MessageModel.find({ chat: req.params.chatId })
+      const fields: any = req.query.fields
+        ? JSON.parse(`${req.query.fields}`)
+        : ["sender", "content", "chat", "updatedAt", "createdAt"];
+      const order_by: any = req.query.order_by
+        ? JSON.parse(`${req.query.order_by}`)
+        : { updatedAt: 1 };
+      const limit: number | string = parseInt(`${req.query.limit}`) || 0;
+      let page: number | string = parseInt(`${req.query.page}`) || 1;
+
+      const search = req.query.search || "";
+
+      let isFilter: any = { $and: [{ chat: req.params.chatId }] };
+
+      if (search) {
+        isFilter = {
+          $and: [{ content: { $regex: search, $options: "i" } }],
+        };
+      }
+      console.log(JSON.stringify(isFilter));
+      // Mengambil hasil fields
+      let setField = FilterQuery.getField(fields);
+      // End
+
+      const getAll = await MessageModel.find(isFilter).count();
+      const messages = await MessageModel.find(isFilter, setField)
+        .sort(order_by)
+        .skip(limit > 0 ? page * limit - limit : 0)
+        .limit(limit > 0 ? limit : getAll)
         .populate("sender", "name pic email")
-        .populate("chat","users");
-      return res.status(200).json({ status: 200, data: messages });
+        .populate("chat", "users");
+
+      return res.status(200).json({
+        status: 200,
+        total: getAll,
+        limit,
+        nextPage: getAll > page * limit && limit > 0 ? page + 1 : page,
+        hasMore: getAll > page * limit && limit > 0 ? true : false,
+        data: messages,
+        filters: stateFilter,
+      });
     } catch (error: any) {
       res.status(400);
       throw new Error(error.message);
